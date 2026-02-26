4607041900f48407f3272d4d1a31567d3cb206a5
Sami <sami.guvenc0@hotmail.com>
2026-02-27 01:00:04 +0300

    Icons fixed

--- data/index.ts
+++ data/index.ts
@@ -90,7 +90,7 @@ export const projects = [
     title: "Laboratuvar Giriş Sistemi ve Akıllı Laboratuvar", // Projenin adı
     des: "Python Flask mimarisi üzerine kurulu, dinamik içerik yönetimi ve backend entegrasyonu sağlayan modern bir web çözümü.",
     img: "/L_Giris.jpg",
-    iconLists: ["/PYTHON.png", "/Flask.png", "/HTML.png", "/CSS.png", "/JS.png"], 
+    iconLists: ["/PYTHON.jpg", "/Flask.png", "/HTML.png", "/CSS.png", "/JS.png"], 
     link: "https://github.com/samigvnc?tab=repositories", // GitHub veya canlı link
   },
 ];
